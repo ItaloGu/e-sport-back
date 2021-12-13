841268269b507bf22431f99f08982a90c376f54a
Guilherme Santos Gomes <guilherme0500@hotmail.com>
2021-12-13 10:23:53 -0300

    criando models

---
+++ models/AssessmentUser.model.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require("mongoose");
+
+const AssessmentUserSchema = new Schema({
+  // Data
+  data: { type: Date},
+  // velocidade
+  velocity: { type: Number, required: true, trim: true },
+  // chute
+  kick: { type: Number, required: true, trim: true },
+  // passe
+  pass: { type: Number, required: true, trim: true },
+  // marcação
+  marking: { type: Number, required: true, trim: true },
+   // Drible
+  dribble: { type: Number, required: true, trim: true },
+  // referencia de qual usuario de refere a avaliação
+  user: [{ type: mongoose.Types.ObjectId, ref: "User" }],
+});
+
+const AssessmentUserModel = model("AssessmentUser", AssessmentUserSchema);
+module.exports = AssessmentUserModel;
---
+++ models/DetailUser.model.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require("mongoose");
+
+const DetailUserSchema = new Schema({
+  // para associar o registro de usuario a tabela 
+  user: [{ type: mongoose.Types.ObjectId, ref: "User" }],
+  // data de nascimento
+  birthday: { type: String, trim: true },
+  // Genero
+  gender: { type: String, trim: true },
+  // Estado
+  district: { type: String, trim: true },
+  // Municipio
+  city: { type: String, trim: true },
+  // ambidestro
+  ambidextrous: { type: String, trim: true },
+  // Goleiro, atacante, zagueiro etc...
+  position: { type: String, trim: true },
+});
+
+const DetailUserModel = model("DetailUser", DetailUserSchema);
+module.exports = DetailUserModel;
