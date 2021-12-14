5f437f0536a97d8f210c435ae333e10d3da2f70d
Guilherme Santos Gomes <guilherme0500@hotmail.com>
2021-12-14 09:52:38 -0300

    rotas

---
+++ routes/assessmentUser.routes.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const router = express.Router();
+const isAuthenticated = require("../middlewares/isAuthenticated");
+const AssessmentUserModel = require("../models/AssessmentUser.model");
+
+// CRUD
+// Crud Create (POST)
+router.post("/assessmentuser/new", isAuthenticated, async (req, res) => {
+  try {
+    console.log(req.body);
+    const assessmentuser = await AssessmentUserModel.create(req.body);
+    res.status(201).json(assessmentuser);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// cRud Read (GET)
+
+router.get("/assessmentuser/list", isAuthenticated, async (req, res) => {
+  try {
+    // Buscar as informações no banco
+    const assessmentuser = await AssessmentUserModel.find();
+    // Responder a requisição
+    res.status(200).json(assessmentuser);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// cRud Read (GET) (Detalhe)
+
+router.get("/assessmentuser/:id", isAuthenticated, async (req, res) => {
+  try {
+    const assessmentuser = await AssessmentUserModel.findOne({ _id: req.params.id });
+    if (!assessmentuser) {
+      return res.status(404).json({ msg: "Avaliação não encontrada." });
+    }
+    res.status(200).json(assessmentuser);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.patch("/assessmentuser/:id", isAuthenticated, async (req, res) => {
+  try {
+
+    const assessmentuser = await AssessmentUserModel.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      { new: true, runValidators: true }
+    );
+
+    if (!assessmentuser) {
+      return res.status(404).json({ msg: "Avaliação não encontrada." });
+    }
+
+    res.status(200).json(assessmentuser);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// cruD Delete (DELETE)
+
+router.delete("/assessmentuser/:id", isAuthenticated, async (req, res) => {
+  try {
+    const assessmentuser = await AssessmentUserModel.deleteOne({ _id: req.params.id });
+    if (assessmentuser.deletedCount < 1) {
+      return res.status(404).json({ msg: "Avaliação não encontrada" });
+    }
+
+    res.status(200).json({});
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
